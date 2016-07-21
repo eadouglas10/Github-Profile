342485dcfe694d437f2a7f93069285316883dbcc
Erik Douglas <eadouglas10@gmail.com>
2016-07-21 14:50:34 -0400

    Links to repos now work

--- repo_client.rb
+++ repo_client.rb
@@ -11,17 +11,21 @@ class RepoClient
 
   #http://api.wunderground.com/api/6fb454176eef8ba8/geolookup/q/20854.json
   def get_repos
-    self.class.get("/#{@username}/repos")
+    @repos = self.class.get("/#{@username}/repos")
+  end
+
+  def get_av_url
+    @repos.first["owner"]["avatar_url"]
   end
 
   def list_repos
     repos = []
-    get_repos.each do |name|
+    @repos.each do |name|
       repos << name["name"]
     end
     repos
   end
 end
 
-#erik = RepoClient.new("eadouglas10")
-#binding.pry
+erik = RepoClient.new("eadouglas10")
+binding.pry
